041ea18315b87e71b39e6b06fefae068ac6e02ed
Kaique Coelho <kaiquescoelho@hotmail.com>
2022-02-14 12:29:50 -0300

    altera descriçao dos testes para ingles

--- tests/tests.spec.js
+++ tests/tests.spec.js
@@ -17,14 +17,14 @@ describe('Tests fetchRandomRecipes', () => {
     fetchRandomRecipes();
     expect(fetch).toHaveBeenCalled();
   })
-  it('expects fetchRandomRecipes to equal "random"', async () => {
+  it('expects fetchRandomRecipes return to match the object random', async () => {
     const request = await fetchRandomRecipes();
-    expect(request).toEqual(random);
+    expect(request).toMatchObject(random);
   })
 });
 
 describe('Tests fetchSearchById', () => {
-  it('verifies if fetchSearchById is a function', () => {
+  it('expects fetchSearchById to be a function', () => {
     expect(typeof fetchSearchById).toBe('function');
   });
 
@@ -33,35 +33,35 @@ describe('Tests fetchSearchById', () => {
     expect(fetch).toHaveBeenCalled();
   });
 
-  it('verifica se fetch foi chamada com a url esperada', async () => {
+  it('expects fetch to have been called with the designated url', async () => {
     const url = `https://api.spoonacular.com/recipes/782619/information?includeNutrition=false&${apiKey}`;
     await fetchSearchById('782619');
     expect(fetch).toHaveBeenCalledWith(url);
   });
 
-  it('verifica se o retorno da função fetchSearchById(""), é uma estrutura de dados igual ao objeto byId', async () => {
+  it('expects fetchSearchById return to match the object byId', async () => {
     const response = await fetchSearchById('782619');
     expect(response).toMatchObject(byId);
   });  
 });
 
-describe('1 - Teste a função fetchSearchByIngredients', () => {
-  it('verifica se fetchSearchByIngredients() é uma função', () => {
+describe('Tests fetchSearchByIngredients', () => {
+  it('expects fetchSearchByIngredients to be a function', () => {
     expect(typeof fetchSearchByIngredients).toBe('function');
   });
 
-  it('verifica se fetch foi chamada ao executar fetchSearchByIngredients(``)', async () => {
+  it('expects fetchSearchByIngredients to call fetch', async () => {
     await fetchSearchByIngredients('potato', undefined, undefined, undefined, 'potato');
     expect(fetch).toHaveBeenCalled();
   });
 
-  it('verifica se fetch foi chamada com a url esperada', async () => {
+  it('expects fetch to have been called with the designated url', async () => {
     const url = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=potato&number=9&${apiKey}&addRecipeInformation=true&fillIngredients=true&intolerances=undefined&diet=undefined&excludeIngredients=undefined&query=potato`;
     await fetchSearchByIngredients('potato', undefined, undefined, undefined, 'potato');
     expect(fetch).toHaveBeenCalledWith(url);
   });
 
-  it('verifica se o retorno da função fetchSearchByIngredients(""), é uma estrutura de dados igual ao objeto byIngredients', async () => {
+  it('expects fetchSearchByIngredients return to match the object byIngredients', async () => {
     const response = await fetchSearchByIngredients('potato', undefined, undefined, undefined, 'potato');
     expect(response).toMatchObject(byIngredients);
   });  
