f149609d5aa1b7a5176ee9cd56cb6250c71595c4
Gabriel Pondaco <gabrielpondaco2013@gmail.com>
2022-02-14 10:01:33 -0300

    isola requests e manipulaçao do DOM

--- script/helpers/requests.js
+++ script/helpers/requests.js
@@ -1,43 +1,23 @@
-const apiKey = 'apiKey=33d37cde403d41ffbe490274e307cb47';
-const input = document.querySelector('#includeIngredients');
+const apiKey = 'apiKey=b46b8b5b592e44d4a9145e4e94b9ec58';
+
 
 const fetchRandomRecipes = async () => {
   const url = `https://api.spoonacular.com/recipes/random?number=9&${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
-  data.recipes.forEach((element) => {
-    const { title, image, readyInMinutes, id } = element;
-    createCard({ title, image, readyInMinutes, id });
-  });
+  return data;
 };
 
-const fetchSearchByIngredients = async () => {
-  document.querySelector('main').innerHTML = '';
-  const querySearch = document.querySelector('#query').value;
-  const intolerancesArr = document.querySelectorAll(
-    'input[name=intolerances]:checked'
-  );
-  const intolerances = Array.from(intolerancesArr)
-    .map((int) => int.id)
-    .join(',');
-  const select = document.querySelector('select');
-  const diet = select[select.selectedIndex].value;
-  const excludeItems = document.querySelector('#excludeIngredients').value;
-  const exclude = excludeItems.split(' ').join(',');
-  const value = input.value;
-  const search = value.split(' ').join(',');
-  const url = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${search}&number=13&${apiKey}&addRecipeInformation=true&fillIngredients=true&intolerances=${intolerances}&diet=${diet}&excludeIngredients=${exclude}&query=${querySearch}`;
+const fetchSearchByIngredients = async (search, intolerances, diet, exclude, querySearch) => {
+  const url = `https://api.spoonacular.com/recipes/complexSearch?includeIngredients=${search}&number=9&${apiKey}&addRecipeInformation=true&fillIngredients=true&intolerances=${intolerances}&diet=${diet}&excludeIngredients=${exclude}&query=${querySearch}`;
   const response = await fetch(url);
   const data = await response.json();
-  data.results.forEach((element) => {
-    createCard(element);
-  });
+  return data;
 };
 
 const fetchSearchById = async (id) => {
   const url = `https://api.spoonacular.com/recipes/${id}/information?includeNutrition=false&${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
   return data;
 };
